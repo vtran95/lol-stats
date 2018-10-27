873ab4d66d5e7428b347786004604b970370eee4
Vincent Tran <vintran1995@gmail.com>
2018-10-27 15:34:19 -0700

    test fix

---
+++ build/precache-manifest.d2e975b9a88f0c89f6df9b9170554312.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "8c2ccbfd94745182baa5",
+    "url": "/static/js/main.8c2ccbfd.chunk.js"
+  },
+  {
+    "revision": "fb42c86e3c7bcdd004bc",
+    "url": "/static/js/1.fb42c86e.chunk.js"
+  },
+  {
+    "revision": "8c2ccbfd94745182baa5",
+    "url": "/static/css/main.c6fd07fe.chunk.css"
+  },
+  {
+    "revision": "62dedb235f629be591b7acc2cf6b6260",
+    "url": "/index.html"
+  }
+];
