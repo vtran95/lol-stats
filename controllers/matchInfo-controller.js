579e338818cd2a93152fef0d7083d41696f37e23
Vincent Tran <vintran1995@gmail.com>
2018-10-28 06:49:13 -0700

    using dragontail pkg

--- controllers/matchInfo-controller.js
+++ controllers/matchInfo-controller.js
@@ -3,6 +3,9 @@ var matchDetails = require ('../models/matchInfo');
 const leagueJs = new LeagueJs(process.env.LEAGUE_API_KEY, {PLATFORM_ID: process.env.LEAGUE_API_PLATFORM_ID});
 const DataDragonHelper = require('../node_modules/leaguejs/lib/DataDragon/DataDragonHelper');
 var XRegExp = require('xregexp');
+var champList = require('../dragontail-6.24.1/6.24.1/data/en_US/champion.json');
+var itemList = require('../dragontail-6.24.1/6.24.1/data/en_US/item.json');
+var sumSpellList = require('../dragontail-6.24.1/6.24.1/data/en_US/summoner.json');
 
 async function show(req, res) {
     var regex = new XRegExp("^[0-9\\p{L} _\\.]+$");
@@ -17,7 +20,7 @@ async function show(req, res) {
         matchHistory.name = summoner.name;
 
         for (const match of matchList) {
-            let getMatch = await leagueJs.Match.gettingById(match.gameId, region);
+            var getMatch = await leagueJs.Match.gettingById(match.gameId, region);
             matches.push(getMatch);
 
             var matchInfo = new matchDetails.matchInfo;
@@ -31,14 +34,25 @@ async function show(req, res) {
                 matchInfo.players.push({playerId: participant.participantId , name: participant.player.summonerName});
             })
             for (var i = 0; i < getMatch.participants.length; i++) {
-                let champ = await leagueJs.StaticData.gettingChampionById(getMatch.participants[i].championId, region);
-                // let champ = await DataDragonHelper.gettingChampionsList(getMatch.participants[i].championId);
+                // let champ = await leagueJs.StaticData.gettingChampionById(getMatch.participants[i].championId, region);
+                var champ = {};
+                for(var champion in champList.data) {
+                    if(champList.data[champion].key == getMatch.participants[i].championId) {
+                        champ = champList.data[champion];
+                        break;
+                    }
+                }
+                if(Object.keys(champ).length === 0) {
+                    champ = await leagueJs.StaticData.gettingChampionById(getMatch.participants[i].championId, region);
+                }
                 matchInfo.players[i].champion = champ.name;
                 matchInfo.players[i].icon = champ.image.full;
                 if (getMatch.participants[i].participantId === mainParticipantId[mainParticipantId.length-1]) {
 
-                    let sumSpell1 = await leagueJs.StaticData.gettingSummonerSpellsById(getMatch.participants[i].spell1Id, region);
-                    let sumSpell2 = await leagueJs.StaticData.gettingSummonerSpellsById(getMatch.participants[i].spell2Id, region);
+                    var sumSpell1 = getSumSpell(sumSpellList, getMatch.participants[i].spell1Id);
+                    // let sumSpell1 = await leagueJs.StaticData.gettingSummonerSpellsById(getMatch.participants[i].spell1Id, region);
+                    var sumSpell2 = getSumSpell(sumSpellList, getMatch.participants[i].spell2Id);
+                    // let sumSpell2 = await leagueJs.StaticData.gettingSummonerSpellsById(getMatch.participants[i].spell2Id, region);
                     if (getMatch.participants[i].stats.perk0) {
                         var keystone = await leagueJs.StaticData.gettingReforgedRuneById(getMatch.participants[i].stats.perk0, region);
                     } else {
@@ -53,7 +67,17 @@ async function show(req, res) {
                     for (var j=0; j<=6; j++) {
                         var item = 'item' + j;
                         if (getMatch.participants[i].stats[item] != 0) {
-                            let itemData = await leagueJs.StaticData.gettingItemById(getMatch.participants[i].stats[item], region);
+                            var itemData = {};
+                            for(var itemId in itemList.data) {
+                                if(itemId == getMatch.participants[i].stats[item]) {
+                                    itemData = itemList.data[itemId];
+                                    break;
+                                }
+                            }
+                            if(Object.keys(itemData).length === 0) {
+                                itemData = await leagueJs.StaticData.gettingItemById(getMatch.participants[i].stats[item], region);
+                            }
+                            // let itemData = await leagueJs.StaticData.gettingItemById(getMatch.participants[i].stats[item], region);
                             matchInfo.items[j] = itemData.name;
                         } else {
                             matchInfo.items[j] = 'Empty';
@@ -101,6 +125,15 @@ async function show(req, res) {
         res.end();
         return;
     }
+}
+
+function getSumSpell(json, id) {
+    for(var spell in json.data) {
+        if(json.data[spell].key == id) {
+            return json.data[spell];
+        }
+    }
+    return;
 }
 
 module.exports = {
